17c67c051b9105e2c5e2f52f17f35226a42a9804
Kalpesh Parab <abhi9405.at@gmail.com>
2025-07-21 13:21:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to UIStacks 🚀</h1>
+      <p>Your personal React component showcase</p>
+    </div>
+  );
+}
+
+export default App;
